b29c9f47fab5c05bdd9645888862ffc6fb6ac7ed
Daniel Kurz <danielkurz03@protonmail.com>
2025-06-25 18:11:09 -0500

    update mysql integration test with enum

--- integration_test/mysql/priv/schema.sql
+++ integration_test/mysql/priv/schema.sql
@@ -3,5 +3,6 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE `users` (
   `id` INT AUTO_INCREMENT PRIMARY KEY,
   `username` VARCHAR(255) NOT NULL UNIQUE,
-  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `status` enum('like', 'neutral')
 );
